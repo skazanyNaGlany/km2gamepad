ab3e2779d2e4749dbd727a25032553287e1facaf
sng <screamingbox@gmail.com>
2019-12-29 21:05:46 +0100

    python_stub update

--- km2gamepad.py
+++ km2gamepad.py
@@ -6,15 +6,16 @@ if __name__ != '__main__':
     exit(1)
 
 
-def install_pip_and_modules(module_names):
-    """
-    Code from https://github.com/skazanyNaGlany/python_stub
-    """
+def install_pip_and_modules(package_names):
     import os
     import os.path
     import sys
-    import importlib
     import shutil
+    import subprocess
+
+    assert sys.version_info >= (2, 7) or sys.version_info >= (3, 4), 'Python 2.7+ or 3.4+ required'
+
+    count_installed_packages = 0
 
     try:
         import urllib2
@@ -44,12 +45,38 @@ def install_pip_and_modules(module_names):
     def determine_install_as_user():
         in_virtualenv = 'VIRTUAL_ENV' in os.environ
         is_root = hasattr(os, 'geteuid') and os.geteuid() == 0
-        is_windows = sys.platform.startswith('win')
 
-        return not in_virtualenv and not is_root and not is_windows
+        return not in_virtualenv and not is_root
+
+    def restart():
+        print('Restarting')
+
+        os.system(sys.executable + ' ' + str(' '.join(sys.argv)))
+        exit(0)
+
+    def get_installed_packages():
+        packages = {}
+
+        output_lines = subprocess.check_output([
+            sys.executable,
+            '-m',
+            'pip',
+            'list'
+        ]).decode('utf-8').split('\n')
+
+        for iline in output_lines[2:]:
+            iline = iline.strip()
+            if not iline:
+                continue
+
+            parts = iline.split(' ')
+            packages[parts[0]] = parts[len(parts) - 1]
+
+        return packages
 
     install_as_user = determine_install_as_user()
 
+    # install pip
     try:
         import pip
     except ImportError as x1:
@@ -69,53 +96,49 @@ def install_pip_and_modules(module_names):
         os.system(cmd)
         os.remove('get-pip.py')
 
+        count_installed_packages += 1
+
         try:
             import pip
         except ImportError:
             print('Unable to install pip')
             exit(1)
 
-    module_names_list = module_names.keys()
+    installed_packages = get_installed_packages()
     cwd = os.getcwd()
 
     # check if we need Dulwich - pure Python Git implementation
     need_dulwich = False
-    for imodule_name2 in module_names:
-        if module_names[imodule_name2].startswith('git+https://'):
+    for ipackage_name2 in package_names:
+        if ipackage_name2.startswith('git+https://'):
             need_dulwich = True
             break
 
     if need_dulwich:
-        try:
-            import dulwich
-        except ImportError as x4:
-            print(x4)
-
+        if not 'dulwich' in installed_packages:
             pip_install_module('dulwich', install_as_user)
+            count_installed_packages += 1
 
-            try:
-                import dulwich
-            except ImportError as x6:
-                print(x6)
+            installed_packages = get_installed_packages()
 
+            if not 'dulwich' in installed_packages:
                 print('Unable to install dulwich')
                 exit(1)
 
-    for imodule_name in module_names_list:
-        try:
-            globals()[imodule_name] = importlib.import_module(imodule_name)
-        except ImportError as x2:
-            print(x2)
+            restart()
 
-            imodule_pip_name = module_names[imodule_name]
+    # install packages
+    for ipackage_name in package_names:
+        imodule_pip_basename = os.path.basename(ipackage_name)
 
-            print('Installing: {} ({})'.format(imodule_name, imodule_pip_name))
+        if not imodule_pip_basename in installed_packages:
+            print('Installing: {} ({})'.format(ipackage_name, ipackage_name))
 
-            if imodule_pip_name.startswith('git+https://'):
+            if ipackage_name.startswith('git+https://'):
                 import dulwich.porcelain
 
                 # just remove git+ and install
-                pkg_url = imodule_pip_name[4:]
+                pkg_url = ipackage_name[4:]
                 pkg_basename = os.path.basename(pkg_url)
 
                 try:
@@ -125,26 +148,33 @@ def install_pip_and_modules(module_names):
 
                 dulwich.porcelain.clone(pkg_url)
                 pip_install_module(pkg_basename, install_as_user)
+                count_installed_packages += 1
 
                 try:
                     shutil.rmtree(os.path.join(cwd, pkg_basename))
                 except Exception as x5:
                     print(x5)
             else:
-                pip_install_module(imodule_pip_name, install_as_user)
-            try:
-                globals()[imodule_name] = importlib.import_module(imodule_name)
-            except ImportError as x3:
-                print(x3)
+                pip_install_module(ipackage_name, install_as_user)
+                count_installed_packages += 1
 
-                print('Unable to install module ' + imodule_name)
-                exit(1)
+    installed_packages = get_installed_packages()
+
+    for ipackage_name2 in package_names:
+        imodule_pip_name2 = os.path.basename(ipackage_name2)
+
+        if imodule_pip_name2 not in installed_packages:
+            print('Unable to install ' + imodule_pip_name2)
+            exit(1)
+
+    if count_installed_packages > 0:
+        restart()
 
 
-install_pip_and_modules({
-    'pynput': 'pynput',
-    'uinput': 'uinput'
-})
+install_pip_and_modules([
+    'pynput',
+    'python-uinput'
+])
 
 
 import uinput
